a8cd28a2d2d42a83fbd4487a6c5d12c593d0695e
RealityVirtual <75819925+RealityVirtual@users.noreply.github.com>
2025-01-31 14:03:51 -0500

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,48 @@
+// Triangle.java
+class Triangle extends Polygon {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    // Getters and Setters
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+    @Override
+    double calculateArea() {
+        double s = calculatePerimeter() / 2; // Semi-perimeter
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+}
